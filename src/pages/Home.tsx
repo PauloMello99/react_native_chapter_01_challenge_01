83775084067629089b8fad88a6b6fb52847fe184
Paulo Mello <paulovmello.99@outlook.com.br>
2022-03-26 11:10:33 -0300

    feat: Add challenge resolution

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+  function handleAddTask(title: string) {
+    const newTask: Task = { id: new Date().getTime(), done: false, title };
+    setTasks((t) => [...t, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks((list) => {
+      const newList = [...list];
+      const taskIndex = newList.findIndex((tsk) => tsk.id === id);
+      newList[taskIndex].done = !newList[taskIndex].done;
+      return newList;
+    });
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((list) => list.filter((tsk) => tsk.id !== id));
   }
 
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
-
       <TodoInput addTask={handleAddTask} />
-
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
